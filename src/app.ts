62c2fbe91932746a91162756535205ffde3961ee
David Sotto Mayor <ddsmmf@gmail.com>
2024-03-31 21:46:55 -0400

    Writing a start point express configs

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,14 @@
-console.log('Hello world!');
+import express from 'express';
+
+// Inicializando o express
+const app = express();
+
+// Configurando body parsers
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
+
+// Servindo app em uma porta
+const port = 8000;
+app.listen(port, () => {
+    console.log(`The app is up and running on port ${port}! 🔥`);
+});
